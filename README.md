1d148777aad05b107af322d21cf4d31c01326335
Joris Rietveld <jorisrietveld@gmail.com>
2016-12-02 20:43:31 +0100

    Initial commit of the project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# HTTP lib
+This repository contains the HTTP library code for the project Digitaal portefolio.
+It provides libraries for object orientated handling of HTTP like the request, response,
+sessions, files and cookies.
+
---
+++ Response.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: Joris Rietveld <jorisrietveld@gmail.com>
+ * Created: 18-11-2016 19:46
+ * Licence: GNU General Public licence version 3 <https://www.gnu.org/licenses/quick-guide-gplv3.html>
+ */
+
+namespace HTTP;
+
+
+class Response
+{
+
+}
